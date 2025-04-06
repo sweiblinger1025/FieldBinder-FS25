3260486ca797299f31cf74ec254a1323d7a76709
sweiblinger1025 <sweiblinger@gmail.com>
2025-04-06 16:59:38 -0500

    Setup Repo

--- core/FieldBinderUtils.lua
+++ core/FieldBinderUtils.lua
@@ -1,12 +1,1 @@
--- File: core/FieldBinderUtils.lua
--- Utility functions for FieldBinder
-
-FieldBinderUtils = {}
-
---- Creates a folder if it doesn't exist
--- @param path string: Full path to the folder
-function FieldBinderUtils.createFolderIfNotExists(path)
-    if not fileExists(path) then
-        createFolder(path)
-    end
-end
+-- Placeholder for FieldBinderUtils.lua
